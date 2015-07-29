1504bd831a2341ad00d5901a3f26a9b71f44c29c
fanitriastowo <fani.triastowo@gmail.com>
2015-07-29 14:37:15 +0700

    add weblookandfeel dependency for UI

--- src/main/java/com/kerjapraktek/dataalumni/Run.java
+++ src/main/java/com/kerjapraktek/dataalumni/Run.java
@@ -3,9 +3,12 @@ package com.kerjapraktek.dataalumni;
 import com.kerjapraktek.dataalumni.view.main.MainFrame;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import com.alee.laf.WebLookAndFeel;
+
 public class Run {
 
     public static void main(String[] args) {
+        WebLookAndFeel.install ();
         AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(SpringConfiguration.class);
         ac.registerShutdownHook();
         MainFrame bean = ac.getBean(MainFrame.class);
